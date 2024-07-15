93a76a2e653c53f9ea2967c53ff5e6f6fbbf909c
User <User@User-PC>
2024-07-15 11:35:23 +0530

    Automation Commit

--- src/batchExecutionClasses/Hayabusa.java
+++ src/batchExecutionClasses/Hayabusa.java
@@ -3,7 +3,12 @@ package batchExecutionClasses;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
-
+//if we want to run all the classes one by one then we go for Batch Execution
+//To run Batch Execution, select all the files and right click
+//And click on convert to TestNG
+// Then give the name file /automation/"filename".xml
+//it will generate a file in test-output folder
+//now click on BatchExecution.xml file and click on run button
 public class Hayabusa {
 	@Test
 	public void hayabusa() {
